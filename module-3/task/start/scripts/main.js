23fda3c15d4e3ce17e1237225f4e17d688623ccd
LolaMcG <lauraamcg@gmail.com>
2021-10-05 20:57:13 +0100

    Update main.js
    Add practice comments, variables, console.log, document.write

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,28 @@
+// This is a comment that's just for me. Secret comment.
+
+/**
+ * This is how
+ * You write
+ * Multiple line comments
+ */
+
+/**
+ * Let age = 37;
+ * Let name = Laura
+ */
+
+/**
+ * Any code that existed before you started coding is called legacy code
+ */
+
+const productName = "broccoli";
+let price = 0.67;
+let quantity = 500;
+let inStock = true;
+let discountAmount = 5;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
