be782e5f6b475b612485ef164f32308b3a16dbc1
LolaMcG <lauraamcg@gmail.com>
2021-11-09 19:06:27 +0000

    Update main.js

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -21,8 +21,20 @@ let quantity = 500;
 let inStock = true;
 let discountAmount = 5;
 
-console.log(productName);
-console.log(price);
-console.log(quantity);
-console.log(inStock);
-console.log(discountAmount);
+console.log("Product name" productName);
+console.log("Price" price);
+console.log("Quantity" quantity);
+console.log("In Stock" inStock);
+console.log("Discount Amount" discountAmount);
+
+/** Putting stuff in quotations like above will display on the console, and is a good way of putting extra info in for people
+ * It's not necessaru but is good practice.
+ */
+
+/**
+ document.write(productName) will display on the browser
+ */
+
+ //console.log(typeof prodctName) will tell you (in the console) what kind of value you will get, be it a number, a string (words) or Boolean (true or false)
+
+ 
