2ced3b66e1de1a07df9df574074ead97f183c100
LolaMcG <lauraamcg@gmail.com>
2021-11-11 21:02:39 +0000

    complete task from JS lesson 1

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -1,40 +1,21 @@
-// This is a comment that's just for me. Secret comment.
+console.log('Hello');
 
-/**
- * This is how
- * You write
- * Multiple line comments
- */
+//Single line comment comes after 2 forward slashes
 
-/**
- * Let age = 37;
- * Let name = Laura
- */
+/*--
+Forward slash and a star is how you start writing multiline comments, and star forward slash is how you end it
+So everything within here is a secret comment
 
-/**
- * Any code that existed before you started coding is called legacy code
- */
+*/
 
-const productName = "broccoli";
-let price = 0.67;
-let quantity = 500;
-let inStock = true;
-let discountAmount = 5;
+let productName = 'Broccoli'
+let price = '40'
+let quantity = '6'
+let inStock = 'true'
+let discountName = '0'
 
-console.log("Product name" productName);
-console.log("Price" price);
-console.log("Quantity" quantity);
-console.log("In Stock" inStock);
-console.log("Discount Amount" discountAmount);
-
-/** Putting stuff in quotations like above will display on the console, and is a good way of putting extra info in for people
- * It's not necessaru but is good practice.
- */
-
-/**
- document.write(productName) will display on the browser
- */
-
- //console.log(typeof prodctName) will tell you (in the console) what kind of value you will get, be it a number, a string (words) or Boolean (true or false)
-
- 
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountName);
