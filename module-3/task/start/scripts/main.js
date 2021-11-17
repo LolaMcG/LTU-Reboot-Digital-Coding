b6e937fc59523587de0c9cfb6e30cecf3773b2b7
LolaMcG <lauraamcg@gmail.com>
2021-11-17 17:48:34 +0000

    Update main.js
    Look at difference between regular functions and arrow functions, and use if else statement to perform calculation

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -1,4 +1,4 @@
-console.log('Hello');
+/* console.log('Hello');
 
 //Single line comment comes after 2 forward slashes
 
@@ -12,10 +12,50 @@ let productName = 'Broccoli'
 let price = '40'
 let quantity = '6'
 let inStock = 'true'
-let discountName = '0'
+let discountAmount = '2'
 
-console.log(productName);
+/*console.log(productName);
 console.log(price);
 console.log(quantity);
 console.log(inStock);
-console.log(discountName);
+console.log(discountName); */
+
+function showProductName(){
+    console.log(productName);
+}
+showProductName();
+
+function totalPrice(productPrice=0,productQuantity=0){
+    productPrice = parseFloat(productPrice);
+    productQuantity = parseFloat(productQuantity);
+    let totalProductPrice = productPrice * productQuantity;
+    console.log(totalProductPrice);
+} 
+
+totalPrice(price,quantity);
+
+//showing the difference between regular function and arrow function
+function squareNumber(number=0){
+    let sum = number * number;
+    return sum;
+}
+
+var square = num => num * num;
+
+let squaredNumber = squareNumber(3);
+console.log(squaredNumber);
+
+let squared = square(3);
+console.log(squared);
+//showing the difference between regular function and squared function
+
+function productDiscount(){
+    if(quantity > 1){
+        sum = price * quantity;
+        let discount = discountAmount * quantity;
+        sum = sum - discount; 
+    }
+    console.log(sum);
+}
+
+productDiscount();
