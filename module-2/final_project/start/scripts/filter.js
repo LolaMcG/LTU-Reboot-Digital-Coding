e6e34640973294f158f6fc53f584180fa7258948
LolaMcG <lauraamcg@gmail.com>
2021-11-30 21:12:43 +0000

    update filter.js
    Add eventSelector and use classes to display or hide certain pictures whether they are selected or not

---
+++ module-2/final_project/start/scripts/filter.js
@@ -0,0 +1,26 @@
+console.log('Laura');
+//let buttons = document.querySelectorAll('button');
+
+let buttons = document.querySelectorAll('.animalFilter');
+let images = document.querySelectorAll('.imageFilter');
+
+//console.log(buttons);
+console.log(images);
+
+buttons.forEach(function(button){
+    //console.log(button);
+    button.addEventListener('click',function(event){
+        let clickedItem = event.target;
+        let animal = clickedItem.getAttribute('animal');
+        console.log(animal);
+        images.forEach(function(image){
+            image.parentElement.classList.add('hide');
+            if(image.classList.contains(animal) || animal == 'all'){
+                //console.log(image);
+                if(image.parentElement.classList.contains('hide')){
+                    image.parentElement.classList.remove('hide');
+                }
+            }
+        })
+    });
+});
